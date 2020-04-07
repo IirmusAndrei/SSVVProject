b71dd947e3d827094cbaaf9d11dc6758db1c64e9
Andrei <andreiirimus@yahoo.com>
2020-04-07 16:10:12 +0300

    test addStudent

--- src/main/java/validation/StudentValidator.java
+++ src/main/java/validation/StudentValidator.java
@@ -6,30 +6,25 @@ public class StudentValidator implements Validator<Student> {
 
     /**
      * Valideaza un student
+     *
      * @param entity - studentul pe care il valideaza
      * @throws ValidationException - daca studentul nu e valid
      */
     @Override
     public void validate(Student entity) throws ValidationException {
-        if(entity.getID().equals("")){
+        if (entity.getID().equals("") || entity.getID() == null) {
             throw new ValidationException("Id incorect!");
         }
-        if(entity.getID() == null){
-            throw new ValidationException("Id incorect!");
-        }
-        if(entity.getNume() == ""){
+
+        if (entity.getNume().equals("") || entity.getNume() == null || entity.getNume().matches(".*\\d.*")) {
             throw new ValidationException("Nume incorect!");
         }
-        if(entity.getGrupa() < 0) {
+
+        if (entity.getGrupa() < 111 || entity.getGrupa() > 937) {
             throw new ValidationException("Grupa incorecta!");
         }
-        if(entity.getEmail() == null){
-            throw new ValidationException("Email incorect!");
-        }
-        if(entity.getNume() == null){
-            throw new ValidationException("Nume incorect!");
-        }
-        if(entity.getEmail().equals("")){
+
+        if (entity.getEmail().equals("") || entity.getEmail() == null || (!entity.getEmail().contains("@") && !entity.getEmail().contains("."))) {
             throw new ValidationException("Email incorect!");
         }
     }
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -2,51 +2,159 @@ package org.example;
 
 import domain.Student;
 import domain.Tema;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import repository.NotaXMLRepo;
+import repository.StudentXMLRepo;
 import repository.TemaXMLRepo;
-import validation.StudentValidator;
-import validation.TemaValidator;
-import validation.ValidationException;
-import validation.Validator;
+import service.Service;
+import validation.*;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 //import static org.junit.jupiter.api.Assertions.*;
 //import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+public class AppTest {
+
+    Service service;
+    StudentXMLRepo studentXMLRepo;
+    StudentValidator studentValidator;
+    TemaXMLRepo temaXMLRepo;
+    TemaValidator temaValidator;
+    NotaXMLRepo notaXMLRepo;
+    NotaValidator notaValidator;
+
+    @Before
+    public void init() {
+        studentXMLRepo = new StudentXMLRepo("fisiere/Studenti.xml");
+        studentValidator = new StudentValidator();
+        temaXMLRepo = new TemaXMLRepo("fisiere/Teme.xml");
+        temaValidator = new TemaValidator();
+        notaXMLRepo = new NotaXMLRepo("fisiere/Note.xml");
+        notaValidator = new NotaValidator(studentXMLRepo, temaXMLRepo);
+        service = new Service(studentXMLRepo, studentValidator, temaXMLRepo, temaValidator, notaXMLRepo, notaValidator);
     }
 
-    public void addAssignmentSuccess(){
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
 
-        TemaXMLRepo repo1 = new TemaXMLRepo("Teme.xml");
-        repo1.save(new Tema("10", "descriere tema 10", 10, 9));
-        assertEquals("descriere tema 10", repo1.findOne("10").getDescriere());
+    @Test
+    public void addStudentSuccess() {
+        service.addStudent(new Student("id", "name", 934, "abc@mail.com"));
+
+        assertEquals("name", service.findStudent("id").getNume());
     }
 
     @Test
-    public void addAssignmentFailure(){
-        Validator<Tema> temaValidator = new TemaValidator();
-        Tema tema = new Tema("11", "", 10, 9);
+    public void addStudent_NullId() {
+        exceptionRule.expect(NullPointerException.class);
 
-        assertTrue(true);
-//        Exception exception = assertThrows(ValidationException.class, () -> {
-//            temaValidator.validate(tema);
-//        });
+        service.addStudent(new Student(null, "name", 934, "abc@mail.com"));
+    }
 
-//        String expectedMessage = "Descriere invalida!";
-//        String actualMessage = exception.getMessage();
+    @Test
+    public void addStudent_EmptyId() {
+        exceptionRule.expect(ValidationException.class);
+        exceptionRule.expectMessage("Id incorect!");
+
+        service.addStudent(new Student("", "name", 934, "abc@mail.com"));
+    }
+
+    @Test
+    public void addStudent_NullName() {
+        exceptionRule.expect(NullPointerException.class);
+
+        service.addStudent(new Student("id", null, 934, "abc@mail.com"));
+    }
+
+    @Test
+    public void addStudent_EmptyName() {
+        exceptionRule.expect(ValidationException.class);
+        exceptionRule.expectMessage("Nume incorect!");
+
+        service.addStudent(new Student("id", "", 934, "abc@mail.com"));
+    }
+
+    @Test
+    public void addStudent_NameContainsNumbers() {
+        exceptionRule.expect(ValidationException.class);
+        exceptionRule.expectMessage("Nume incorect!");
+
+        service.addStudent(new Student("id", "nume1", 934, "abc@mail.com"));
+    }
+
+    @Test
+    public void addStudent_NullGroup() {
+        exceptionRule.expect(NumberFormatException.class);
+
+        service.addStudent(new Student("id", "nume1", Integer.parseInt(null), "abc@mail.com"));
+    }
+
+    @Test
+    public void addStudent_GroupNotNaturalNumber() {
+        exceptionRule.expect(NumberFormatException.class);
+
+        service.addStudent(new Student("id", "nume", Integer.parseInt("934.5"), "abc@mail.com"));
+    }
+
+    @Test
+    public void addStudent_GroupOutOfBoundary() {
+        exceptionRule.expect(ValidationException.class);
+        exceptionRule.expectMessage("Grupa incorecta!");
+
+        service.addStudent(new Student("id", "nume", 110, "abc@mail.com"));
+    }
+
+    @Test
+    public void addStudent_NullEmail() {
+        exceptionRule.expect(NullPointerException.class);
+
+        service.addStudent(new Student("id", "nume", 934, null));
+    }
+
+    @Test
+    public void addStudent_EmptyEmail() {
+        exceptionRule.expect(ValidationException.class);
+        exceptionRule.expectMessage("Email incorect!");
+
+        service.addStudent(new Student("id", "nume", 934, ""));
+    }
+
+    @Test
+    public void addStudent_EmailWrongFormat() {
+        exceptionRule.expect(ValidationException.class);
+        exceptionRule.expectMessage("Email incorect!");
+
+        service.addStudent(new Student("id", "nume", 934, "email"));
+    }
+
+//    @Test
+//    public void addAssignmentSuccess() {
 //
-//        assertTrue(actualMessage.contains(expectedMessage));
-    }
+//        TemaXMLRepo repo1 = new TemaXMLRepo("fisiere/Teme.xml");
+//        repo1.save(new Tema("10", "descriere tema 10", 10, 9));
+//        assertEquals("descriere tema 10", repo1.findOne("10").getDescriere());
+//    }
+//
+//    @Test
+//    public void addAssignmentFailure() {
+//        Validator<Tema> temaValidator = new TemaValidator();
+//        Tema tema = new Tema("11", "", 10, 9);
+//
+//        assertTrue(true);
+////        Exception exception = assertThrows(ValidationException.class, () -> {
+////            temaValidator.validate(tema);
+////        });
+//
+////        String expectedMessage = "Descriere invalida!";
+////        String actualMessage = exception.getMessage();
+////
+////        assertTrue(actualMessage.contains(expectedMessage));
+//    }
 }
